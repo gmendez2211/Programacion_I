e4a42bc6baf561c11ddc9c4506bc20f0a29ae168
Gustavo Mendez <56131271+gmendez2211@users.noreply.github.com>
2024-02-13 22:51:58 -0600

    Ciclo While

---
+++ CicloWhile/CicloWhile/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CicloWhile
+//
+//  Created by Gustavo Méndez on 13/02/24.
+//
+
+#include <iostream>
+
+int nCiclo=0;
+
+int main(int argc, const char * argv[]) {
+    while (nCiclo<10){
+        std::cout << nCiclo<<"\n";
+        nCiclo++;
+    }
+    return 0;
+}
